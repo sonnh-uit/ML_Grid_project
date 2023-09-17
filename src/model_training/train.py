e91df1ae64fd9ac3d10ae917e1cb10dff3694cc5
sonnh-uit <sonnh.uit@gmail.com>
2023-09-17 11:59:55 +0700

    add reference for training pipeline

--- src/model_training/train.py
+++ src/model_training/train.py
@@ -25,7 +25,34 @@ def pickle_save_model(model, model_path: str):
 
     return pickle.dump(model, open(model_path, 'wb'))
 
+def evaluate(test_data,prediction_data,validate_data,prediction_validate_data):
 
+    mean_absolute_error = mean_absolute_error(test_data, prediction_data)
+    test_ape = np.abs((test_data - prediction_data) / test_data)
+    test_mape = np.mean(test_ape) * 100
+    test_mse = mean_squared_error(test_data, prediction_data)
+    test_rmse = np.sqrt(test_mse)
+
+    val_mae = mean_absolute_error(validate_data, prediction_validate_data)
+    val_ape = np.abs((validate_data - prediction_validate_data) / validate_data)
+    val_mape = np.mean(val_ape) * 100
+    val_mse = mean_squared_error(validate_data, prediction_validate_data)
+    val_rmse = np.sqrt(val_mse)
+    
+    metadata = {
+        "test_result" : {
+            "mean_absolute_error" : mean_absolute_error,
+            "mean_absolute_percentage_error" : test_mape,
+            "root_mean_squared_error" : test_rmse,
+        },
+        "validate_result" : {
+            "mean_absolute_error" : val_mae,
+            "mean_absolute_percentage_error" : val_mape,
+             "root_mean_squared_error" : val_rmse
+        }
+    }
+
+    return metadata
 
 # def run():
 
